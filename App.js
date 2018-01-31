23139920809774200eac8ef8ac14d005d6a776f1
Arthnick <arthnick@Mr.Man>
2018-01-30 19:52:41 -0500

    React-Native Project Albums

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Header headerText={'Albums'} />
+        <AlbumList />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff'
+  },
+});
